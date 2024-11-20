daf469a1d0caa048e50a23d3b5c363602b24450f
Sebastian Knap <sekn.kn@gmail.com>
2024-11-20 22:01:08 +0100

    add layered encryption LSB

--- src/modes/all-modes.ts
+++ src/modes/all-modes.ts
@@ -1,6 +1,7 @@
 import lsb from './lsb-random'
 import twoTwoFourLsb from './2-2-4'
 import lsbAES from './lsb-aes'
+import encryptionLayeredLSB from './encryption-layered-lsb'
 import { Mode } from './template'
 
-export default [lsb, twoTwoFourLsb, lsbAES] satisfies Array<Mode>
+export default [lsb, twoTwoFourLsb, lsbAES, encryptionLayeredLSB] satisfies Array<Mode>
